d656ebe9aafe13ba39fe28e41dea239518e75011
Starz0r <starz0r@starz0r.com>
2024-08-09 17:01:40 -0500

    i don't know anymore

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ BACKGROUND_JOBS: Set[asyncio.Task[None]] = set()
 
 
 def upload_manifest(manifest: List[Any]) -> str:
-    finfo = ("".join(random.choice(string.ascii_lowercase) for i in range(5)), str.encode(json.dumps(manifest), encoding="utf-8"), "json")
+    finfo = (str.encode(json.dumps(manifest), encoding="utf-8"), "application/json")
     resp = requests.post(
         "https://litterbox.catbox.moe/resources/internals/api.php",
         data={
@@ -35,6 +35,7 @@ def upload_manifest(manifest: List[Any]) -> str:
         headers={
             "User-Agent": "AWACY Python Manifest Exporter",
         },
+        files=str.encode(json.dumps(manifest)),
         stream=True,
     )
     if resp.ok:
